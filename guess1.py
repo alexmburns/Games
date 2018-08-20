6ebf7296843563cc16d7027db84150cd611ca428
Alex Burns <30638935+alexmburns@users.noreply.github.com>
2018-08-20 20:58:08 +0100

    Update guess1.py

--- guess1.py
+++ guess1.py
@@ -3,7 +3,7 @@ import random
 guessesTaken = 0
 
 print('Hello! What is your name?')
-myName = input()
+myName = raw_input()
 
 number = random.randint(1, 20)
 print('Well, ' + myName + ', I am thinking of a number between 1 and 20.')
